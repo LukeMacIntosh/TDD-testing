8d1eba153472536924b3622f342cf5b7364d9355
Luke <Luke@DESKTOP-L9H7AQ0>
2019-10-13 10:36:16 -0400

    code created for first test (within triangle class)

--- SQ - TDD/Triangle.h
+++ SQ - TDD/Triangle.h
@@ -1,4 +1,14 @@
 #pragma once
 
 class Triangle {
+public:
+	int sideA;
+	int sideB;
+	int sideC;
+
+	int CalcHypotenus(int sideA, int sideB) {
+		sideC = sqrt((sideA * sideA) + (sideB * sideB));
+		return sideC;
+	}
+
 };
